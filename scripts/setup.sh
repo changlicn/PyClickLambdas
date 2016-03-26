236c8a15dc80ae7127e3aad509c906f93e1f9fcd
Tomáš Tunys <tunystom@gmail.com>
2016-03-26 23:28:58 +0100

    Updated setup for building the user and bandit modules.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,9 +8,9 @@ import numpy as np
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(ext_modules=cythonize(["./samplers/samplers_inner.pyx", "./users/users_inner.pyx"]), include_dirs=np.get_include())
+setup(ext_modules=cythonize(["./samplers/samplers_inner.pyx", "./users/users_inner.pyx", "./rankbs/bandits.pyx"]), include_dirs=np.get_include())
 END
 
 python ${TMPFILE} build_ext --inplace
 
-rm -rf build/ samplers/samplers_inner.c users/users_inner.c ${TMPFILE}
+rm -rf build/ samplers/samplers_inner.c users/users_inner.c rankbs/bandits.c ${TMPFILE}
