9430617e0eae035fa0b3fe8a7c9f24d2bbd18806
unknown <m.zoghi@uva.nl>
2016-04-17 22:46:21 +0200

    CoarseRelativeRankingAlgorithm

--- scripts/RankingBanditAlgorithm.py
+++ scripts/RankingBanditAlgorithm.py
@@ -510,3 +510,217 @@ class RelativeRankingAlgorithm(BaseLambdasRankingBanditAlgorithm):
                     print "N =",N
                     print "(topK,bottomK) =", (topK,bottomK)
                     raise ValueError,"Unexpected non-conforming index"
+
+
+
+class CoarseRelativeRankingAlgorithm(BaseLambdasRankingBanditAlgorithm):
+
+    def __init__(self, *args, **kwargs):
+        super(RelativeRankingAlgorithm, self).__init__(*args, **kwargs)
+        try:
+            self.t = 1
+            self.alpha = kwargs['alpha']
+            self.C = []
+        except KeyError as e:
+            raise ValueError('missing %s argument' % e)
+        self.shuffler = UniformRankingSampler(np.empty(self.n_documents,
+                                                     dtype='float64'),
+                                            random_state=self.random_state)
+
+        # Validate the type of the feedback model.
+        if not isinstance(self.feedback_model,
+                          ClickLambdasAlgorithm.RefinedSkipClickLambdasAlgorithm):
+            raise ValueError('expected RefinedSkipClickLambdasAlgorithm for '
+                             'feedback_model but received %s'
+                             % type(self.feedback_model))
+
+    @classmethod
+    def update_parser(cls, parser):
+        super(RelativeRankingAlgorithm, cls).update_parser(parser)
+        parser.add_argument('-a', '--alpha', type=float, default=0.51,
+                            required=True, help='alpha parameter')
+        parser.add_argument('-g', '--gamma', type=float, required = True,
+                            help='continuation probability lower bound')
+
+    @classmethod
+    def getName(cls):
+        '''
+        Returns the name of the algorithm.
+        '''
+        return 'RelativeRankingAlgorithm'
+
+    def get_chain_in(self, P_t):
+        # The number of (other) documents beating each document.
+        n_beating_d = P_t.sum(axis=0)
+
+        # Queue keeps documents in the order in which they
+        # should be ranked.
+        queue = np.where(n_beating_d == 0)[0].tolist()
+
+        # There cannot be more than 1 unbeaten document
+        # if we are looking for a single chain.
+        if len(queue) > 1:
+            return []
+
+        # Topological order (preference ordering) of
+        # vertices (documents) in the graph induced
+        # by P_t (preferences).
+        chain = []
+
+        indicator = np.zeros(self.n_documents,dtype="bool")
+
+        for d in queue:
+            indicator[d] = True
+
+        while len(queue) > 0:
+            u = queue.pop(0)
+            for v in xrange(self.n_documents):
+                n_beating_d[v] -= P_t[u, v]
+                if not indicator[v] and n_beating_d[v] == 0:
+                    queue.append(v)
+                    indicator[v] = True
+            chain.append(u)
+            if len(chain) == self.cutoff:
+                break
+
+        # A preference cycle was detected?
+        if len(chain) != self.cutoff:
+            return []
+
+        for d in (set(range(self.n_documents)) - set(chain)):
+            chain.append(d)
+
+        # Check there is total ordering in top K documents,
+        # (if not return empty array) ...
+        for i in xrange(self.cutoff - 1):
+            if P_t[chain[i], chain[i + 1]] != 1:
+                return []
+
+        # ... and the K-th document beats all
+        # the lower ranked documents...
+        for i in xrange(self.cutoff, self.n_documents):
+            if P_t[chain[self.cutoff - 1], chain[i]] != 1:
+                return []
+
+        # ... return the chain if all conditions
+        # above are satisfied.
+        return np.array(chain[:self.cutoff], dtype='int32')
+
+    def detected_loops_in(self, P_t):
+        return False
+
+    def get_ranking(self, ranking):
+        # Get the required statistics from the feedback model.
+        Lambdas, N = self.feedback_model.statistics()
+
+        # Number of query documents and cutoff are available field variables.
+        L = self.n_documents
+        K = self.cutoff
+
+        # Keep track of time inside the model. It is guaranteed that the pair
+        # of methods get_ranking and set_feedback is going to be called
+        # in this order each time step.
+        self.t += 1
+
+        # Whenever you would like to restart the algorithm, call
+        # self.feedback_model.reset().        
+
+        # Put the ranking produced by the algorithm into ranking,
+        # which is an array of ints with shape = [self.n_documents].
+        ranking[:] = np.arange(L, dtype='int32')
+
+        # It is not needed to create rankings of size L, you can only
+        # set the top K documents, the rest of documents will not be
+        # 'seen' by the click model.
+
+        # Sanity check that the arrays are in order klij.
+        if Lambdas.shape == (K, K, L, L):
+            raise ValueError('misordered dimension in lambdas and counts')
+
+        # Lambda_ij is the same as Lambdas
+        Lambda_ij = Lambdas
+
+        # Lambda_ji is the transpose of Lambda_ij. This operation
+        # is very cheap in NumPy >= 1.10 because only a view needs
+        # to be created.
+        Lambda_ji = np.swapaxes(Lambda_ij, 0, 1)
+
+        # N_ij is the same as N.
+        N_ij = N
+
+        # N_ji is the transpose of N_ij. Similarly to construction
+        # of Lambda_ji this can turn out to be very cheap.
+        N_ji = np.swapaxes(N_ij, 0, 1)
+
+        # p is the frequentist mean.
+        p = Lambda_ij / N_ij - Lambda_ji / N_ji
+
+        # c is the size of the confidence interval
+        c = (np.sqrt(self.alpha * np.log(self.t + 1) / N_ij) + 
+             np.sqrt(self.alpha * np.log(self.t + 1) / N_ji))
+
+        # Get LCB.
+        LCB = p - c
+
+        # The partial order.
+        P_t = (LCB > 0).any(axis=(2, 3))
+
+        # if detected_loops_in(P_t):
+        #     # TT: What should happen here?
+
+        if self.C != []:
+            topK = P_t[self.C[1:K],self.C[:K-1]]
+            notInC = sorted(set(range(L))-set(self.C))
+            bottomK = P_t[notInC,self.C[K-1]]
+            if topK.any() or bottomK.any():
+                self.C = []
+                self.feedback_model.reset()
+
+        if self.C == []:
+            chain = self.get_chain_in(P_t)
+            if chain != []:
+                self.C = chain
+                ranking[:K] = self.C[:K]
+                return ranking
+            else:
+                return self.shuffler.sample(ranking)
+        else:
+            topK = P_t[self.C[:K-1],self.C[1:K]]
+            notInC = sorted(set(range(L))-set(self.C))
+            bottomK = P_t[self.C[K-1],notInC]
+            if topK.all() and bottomK.all():
+                try:
+                    ranking[:K] = self.C
+                except ValueError:
+                    print "self.C =",self.C
+                    print "ranking =",ranking
+                    a = badvar
+                return ranking
+            else:
+                N = np.nonzero(topK)[0].tolist()+\
+                    (np.nonzero(bottomK)[0]+K).tolist()
+                k = self.random_state.choice(N)
+                if k<K-1:
+                    if self.random_state.rand() < 0.5:
+                        ranking[:K] = self.C
+                        return ranking
+                    else:
+                        ranking[:K] = self.C[:]
+                        ranking[k] = self.C[k+1]
+                        ranking[k+1] = self.C[k]
+                        return ranking
+                elif k>K-1:
+                    if self.random_state.rand() < 0.5:
+                        ranking[:K] = self.C
+                        ranking[K-2] = notInC[k-K]
+                        return ranking
+                    else:
+                        ranking[:K] = self.C[:]
+                        ranking[K-2] = self.C[K-1]
+                        ranking[K-1] = notInC[k-K]
+                        return ranking
+                else:
+                    print "(k,K) =",(k,K)
+                    print "N =",N
+                    print "(topK,bottomK) =", (topK,bottomK)
+                    raise ValueError,"Unexpected non-conforming index"
